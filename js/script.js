269fb0cb5199da435f348812c7d9a304e2fdfd46
CodeSevenic <sevenicbookings@gmail.com>
2022-10-01 15:53:23 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+﻿const burgerBar = document.getElementById('mobi-bar');
+const ul_enu = document.querySelector('header nav .menu-items');
+
+addEventListener('resize', function () {
+  if (ul_enu.classList.contains('active')) {
+    ul_enu.classList.remove('active');
+  }
+});
+
+burgerBar.addEventListener('click', function (e) {
+  if (!ul_enu.classList.contains('active')) {
+    ul_enu.classList.add('active');
+  } else {
+    ul_enu.classList.remove('active');
+  }
+});
